b4b4347876f535dca1ab8f1f073b6acceb93ff7b
+ <kibetg984@gmail.com>
2024-10-13 23:00:10 +0300

    Updated the javascript code for each class of code

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your code here
+// Define the Breakfast class
+class Breakfast {
+    constructor(food, drink) {
+      this.food = food;
+      this.drink = drink;
+    }
+  }
+  
+  // Define the Lunch class
+  class Lunch {
+    constructor(salad, soup, drink) {
+      this.salad = salad;
+      this.soup = soup;
+      this.drink = drink;
+    }
+  }
+  
+  // Define the Dinner class with a private #dessert field
+  class Dinner {
+    #dessert; // Declare the private field for dessert
+    
+    constructor(salad, soup, entree, dessert) {
+      this.salad = salad;
+      this.soup = soup;
+      this.entree = entree;
+      this.#dessert = dessert; // Assign to the private field
+    }
+  
+    // Optional getter for dessert if needed
+    getDessert() {
+      return this.#dessert;
+    }
+  }
+  
+  // Export the classes
+  module.exports = { Breakfast, Lunch, Dinner };
+  
